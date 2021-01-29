3d47f09da7344fcbe16c9ffb8b99ee4f65960793
Joe Tustin <joetustin@joetustin.home>
2021-01-29 16:41:28 -0700

    Adding in new models

--- graphql/resolvers.js
+++ graphql/resolvers.js
@@ -1,10 +1,33 @@
+const bcrypt = require('bcryptjs')
+
+const User = require('../models/user')
+
 module.exports = {
-    createUser(args, req) {
+    createUser: async function(args, req) {
         const email = args.userInput.email
+        const existingUser = await User.findOne({email: userInput.email})
+
+        if (existingUser) {
+            const error = new Error('User exists already')
+            throw error
+        }
+
+        const hashedPw = await bcrypt.hash(userInput.password, 12)
+        const user = new User({
+            email: userInput.email,
+            name: userInput.name,
+            password: hashedPw
+        })
+        const createdUser = await user.save()
         
+        return {...createdUser._doc, _id: createdUser._id.toString()}
 
-
+    
     }
+
+
+
+
 }
 // module.exports = {
 //     hello() {
